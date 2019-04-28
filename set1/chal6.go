474d7a70f678ce4a0de3ec2d8b10eb4857d02e39
ngrande <devlup@outlook.com>
2019-04-28 22:00:58 +0200

    Key generator still broken

--- set1/chal6.go
+++ set1/chal6.go
@@ -7,6 +7,8 @@ import "bufio"
 import b64 "encoding/base64"
 import "strings"
 
+var key_gen_sep byte = 1
+
 func calc_diff(left string, right string) int {
 	diff := 0
 
@@ -17,12 +19,15 @@ func calc_diff(left string, right string) int {
 	return diff
 }
 
-func detect_key_size(text string, min int, max int) int {
+func detect_key_size(text string, min int, max int) []int {
 
 	//slice := text[:]
 	best_score := 10000000
-	best_size := 0
+	var res []int
 	for possible := min; possible <= max; possible++ {
+		if (possible * 2 > len(text)) {
+			break
+		}
 		first_slice := text[:possible]
 		second_slice := text[possible:possible * 2]
 		if len(first_slice) != possible || len(second_slice) != possible {
@@ -33,33 +38,49 @@ func detect_key_size(text string, min int, max int) int {
 		edit_distance := calc_diff(first_slice, second_slice)
 		normalized := edit_distance / possible
 		fmt.Printf("d: %d, n: %d, key_size: %d\n", edit_distance, normalized, possible)
-		if (normalized < best_score) {
-			best_size = possible
+		if (normalized <= best_score) {
 			best_score = normalized
+			res = append(res, []int{ possible }...)
 		}
 	}
 
-	return best_size
+	return res 
 }
 
 func is_valid_char(in byte) bool {
-	lower := strings.ToLower(string(in))
-	if strings.Contains("abcdefghijklmnopqrstuvwxyz\"\n&()!=? ", lower) {
+	// all printable ASCII chars
+	if in >= 32 && in <= 126 {
+		if len(string(in)) == 0 {
+			panic("Not a valid char!")
+		}
 		return true
 	}
-
 	return false
+
+//	lower := strings.ToLower(string(in))
+//	if strings.Contains("abcdefghijklmnopqrstuvwxyz\"\n&()!=? ,", lower) {
+//		return true
+//	}
+//
+//	return false
 }
 
 func check_all_blocks(text string, key_size int, key_ind int, candidate byte) bool {
 	// go through all blocks
 	for i := 0; i < len(text); i+= key_size {
-		block := text[i:i+key_size]
-		encr := block[key_ind]
-		decr := encr ^ candidate
+		end := i + key_size
+		if end > len(text) {
+			end = len(text)
+		}
+		block := text[i:end]
 
-		if is_valid_char(decr) == false {
-			return false
+		if key_ind + i < len(text) {
+			encr := block[key_ind]
+			decr := encr ^ candidate
+
+			if is_valid_char(decr) == false {
+				return false
+			}
 		}
 	}
 
@@ -69,14 +90,24 @@ func check_all_blocks(text string, key_size int, key_ind int, candidate byte) bo
 
 func crypt(text string, key string) string {
 	key_len := len(key)
+	if key_len == 0 {
+		panic("Key len can not be 0!")
+	}
 
 	var result string
 
 	for i := 0; i < len(text); i+= key_len {
-
-		block := text[i:i + key_len]
+		end := i + key_len
+		if end > len(text) {
+			end = len(text)
+		}
+		block := text[i:end]
 
 		for k := 0; k < key_len; k++ {
+			if (k + i >= len(text)) {
+				// text block < key_len
+				break
+			}
 			result += string(block[k] ^ key[k])
 		}
 	}
@@ -87,10 +118,15 @@ func crypt(text string, key string) string {
 func score(in string) int {
 	sc := 0
 
-	common_chars := "zqxjkvbpygfwmucldrhsnioate"
+	common_chars := "!,zqxjkvbpygfwmucldrhsnioate "
 
 	for _, char := range in {
 		
+		if is_valid_char(byte(char)) == false {
+			fmt.Println("Wrong score: ", in)
+			return -1
+		}
+
 		for worth, comm := range common_chars {
 			if char == comm || byte(char) == byte(comm) - 22 {
 				sc += worth
@@ -102,8 +138,10 @@ func score(in string) int {
 }
 
 func score_text(text string, key string) (int, string) {
+	fmt.Println("Decrypting with key ", key, " text: ", text)
 	decrypted := crypt(text, key)
 
+	fmt.Println("Decrypted")
 	sc := score(decrypted)
 	return sc, decrypted
 }
@@ -141,7 +179,7 @@ func comb(in []string) string {
 			tmp += comb(in[1:])
 		}
 
-		results += tmp + " "
+		results += tmp + string(key_gen_sep)
 	}
 
 	return results
@@ -150,7 +188,7 @@ func comb(in []string) string {
 func generate(data []string) []string {
 	in := comb(data)
 
-	split := strings.Split(in, " ")
+	split := strings.Split(in, string(key_gen_sep))
 
 //	fmt.Println(split)
 
@@ -160,13 +198,13 @@ func generate(data []string) []string {
 	group := split[0]
 	result := ""
 	for i := 0; i < len(split); i++ {
-		if strings.Contains(split[i], " ") || len(split[i]) == 0 {
+		if strings.Contains(split[i], string(key_gen_sep)) || len(split[i]) == 0 {
 			continue
 		}
 //		fmt.Println("Split: ", split[i])
 		if len(split[i]) == group_len {
 			group = split[i]
-			fmt.Println(group)
+//			fmt.Println(group)
 			result += group + " "
 			continue
 		}
@@ -184,12 +222,12 @@ func generate(data []string) []string {
 	return strings.Split(result, " ")
 }
 
-func crack_key(text string, key_size int) string {
+func crack_key(text string, key_size int) (int, string) {
 	var key string
 
 	key_cand := search_key_candidates(text, key_size)
 	for cand := range key_cand {
-		fmt.Printf("Candidates #%d: %s\n", cand, key_cand[cand])
+		fmt.Printf("Candidates #%d: '%s'\n", cand, key_cand[cand])
 	}
 	// key_cand looks like:
 	// "HELLO"
@@ -211,12 +249,14 @@ func crack_key(text string, key_size int) string {
 
 	fmt.Printf("Possible keys (%d): %s\n", len(keys_arr), keys_arr)
 
-	high_score := 0
+	high_score := -100000
 	high_score_text := ""
 	for i := 0; i < len(keys_arr); i++ {
 		pos_key := keys_arr[i]
 
+		fmt.Println("Scoring...")
 		sc, decr := score_text(text, pos_key)
+		fmt.Println("Text scored: ", decr)
 		if sc > high_score {
 			high_score = sc
 			high_score_text = decr
@@ -225,7 +265,7 @@ func crack_key(text string, key_size int) string {
 		}
 	}
 
-	return key
+	return high_score, key
 }
 
 func main() {
@@ -242,7 +282,7 @@ func main() {
 	}
 
 
-	input_test := "hello you"
+	input_test := "hello, my dear friend!"
 	encrypted_test := crypt(input_test, "lol")
 	decrypted_test := crypt(encrypted_test, "lol")
 
@@ -261,20 +301,25 @@ func main() {
 		text += scanner.Text()
 	}
 
+	// ACTIVATE LATER
 	enc, _ := b64.StdEncoding.DecodeString(text)
 	decoded := string(enc)
-	//fmt.Println(decoded)
 
-	//fmt.Println(text)
+	if decoded == text {
+		fmt.Println("LOL")
+	}
 
-	key_size := detect_key_size(decoded, 2, 40)
+//	key_size := detect_key_size(decoded, 2, 40)
 
-	fmt.Println("Keysize detected: ", key_size)
+//	fmt.Println("Keysize detected: ", key_size)
 
 //	crack_key(decoded, key_size)
 
+	key_sizes := detect_key_size(encrypted_test, 2, 40)
+	for i := 0; i < len(key_sizes); i++ {
+		fmt.Println("Checking for key size: ", key_sizes[i])
+		key_score, key := crack_key(encrypted_test, key_sizes[i])
 
-	key := crack_key(encrypted_test, 3)
-
-	fmt.Println("Found key: ", key)
+		fmt.Printf("Found key: '%s' with score: %d\n", key, key_score)
+	}
 }
